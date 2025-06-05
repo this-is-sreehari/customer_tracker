59a2042515b6d35a0af91dcb3616d7854435c9b9
this-is-sreehari <sreeharipathil@gmail.com>
2025-06-05 19:56:50 +0530

    refact: comment port field for db connection object

--- src/db.ts
+++ src/db.ts
@@ -8,7 +8,7 @@ dotenv.config();
 // database connection
 const connection = mysql.createConnection({
     host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT || '3306'),
+    // port: parseInt(process.env.DATABASE_PORT || '3306'),
     user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
